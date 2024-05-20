45f4d348ca646e549080af30a7aa99275a8ff1aa
vcarla <carla.vekony@student.upt.ro>
2024-05-20 16:30:24 +0300

    admin page list of emails with rights

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,3 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import { db } from "../../config/firebase-config";
+import { collection, getDocs, where, query } from "firebase/firestore";
+
 export const Admin = () => {
-    return <div> admin page </div>;
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const userRef = collection(db, "User");
+                const q = query(userRef, where("rights", "==", true));
+                const querySnapshot = await getDocs(q);
+                const userData = [];
+                querySnapshot.forEach((doc) => {
+                    userData.push(doc.data());
+                });
+                setUsers(userData);
+                setLoading(false);
+            } catch (error) {
+                console.error("Error fetching users:", error);
+            }
+        };
+
+        fetchUsers();
+    }, []);
+
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+
+    return (
+        <div>
+            <h1>Admin Page</h1>
+            <table>
+                <thead>
+                <tr>
+                    <th>Email of users with rights</th>
+                </tr>
+                </thead>
+                <tbody>
+                {users.map((user, index) => (
+                    <tr key={index}>
+                        <td>{user.email}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
 };
