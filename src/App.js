2d3a8aa0abe0f54c5bfbf882c959030787dc2315
beriboss2 <sergiu.a.berindeiu@gmail.com>
2024-05-20 13:32:25 +0300

    login

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import "./App.css"
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import {Auth} from "./pages/auth/index";
-import {TravelApp} from "./pages/travelApp/index";
-import {SignUp} from "./pages/signUp/index";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Auth } from "./pages/auth/index";
+import { TravelApp } from "./pages/travelApp/index";
+import { SignUp } from "./pages/signUp/index";
 
 function App() {
-  return(
-      <div className="App">
-          <Router>
-            <Routes>
-              <Route path="/" exact element={< Auth />} />
-               <Route path = "/travel-application" element={< TravelApp />} />
-                <Route path = "/sign-up" element={< SignUp />} />
-            </Routes>
-          </Router>
-      </div>
-  );
+    return (
+        <div className="App">
+            <div className="background-slide blur"></div>
+            <div className="background-slide blur"></div>
+            <div className="background-slide blur"></div>
+            <div className="background-slide blur"></div>
+            <div className="background-slide blur"></div>
+            <div className="background-slide blur"></div>
+            <Router>
+                <div className="content">
+                    <Routes>
+                        <Route path="/" exact element={<Auth />} />
+                        <Route path="/travel-application" element={<TravelApp />} />
+                        <Route path="/sign-up" element={<SignUp />} />
+                    </Routes>
+                </div>
+            </Router>
+        </div>
+    );
 }
-
 
 export default App;
